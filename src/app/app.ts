405228bbd1453cf6a5ce3712f7ee4bf61a1eadc4
moamenamgad-Work <maomenamgadw@gmail.com>
2025-12-15 20:42:14 +0200

    greate Contact Us ( moamen T-2 )

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Contact } from './features/contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Contact],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('doctor-appointment-team1');
